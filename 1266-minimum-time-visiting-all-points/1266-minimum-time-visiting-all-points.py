84276aafd998d85656ea9ac873a0e1b01b4af4d0
John Carlo Laude <61213741+JohnLaude@users.noreply.github.com>
2022-07-30 10:51:56 +0800

    Time: 120 ms (9.44%), Space: 13.4 MB (52.78%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        time = 0
+        n = 0
+        m = 1
+        while m < len(points): 
+            diff_x = points[n][0] - points[m][0]
+            diff_y = points[n][1] - points[m][1]
+            if diff_y < 0:
+                diff_y *= -1
+            if diff_x < 0:
+                diff_x *= -1
+            temp = max(diff_x, diff_y)
+            time += temp
+            print(time)
+            m += 1
+            n += 1
+            
+        return time
+            
+        
